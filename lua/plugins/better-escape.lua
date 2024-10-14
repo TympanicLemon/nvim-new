f59b234780f8d89c07ba66fb6bbe4c3dad61fa5b
Daniel Zapata <danielz@Daniels-MBP-2.attlocal.net>
2024-10-14 12:28:36 -0700

    added lazygit plugin, also changed some stuff to enable lazygit to with betterescape

--- lua/plugins/better-escape.lua
+++ lua/plugins/better-escape.lua
@@ -2,7 +2,23 @@ return {
 	{
 		"max397574/better-escape.nvim",
 		config = function()
-			require("better_escape").setup()
+			require("better_escape").setup({
+				default_mappings = false,
+				mappings = {
+					i = {
+						j = {
+							k = "<Esc>",
+							j = "<Esc>",
+						},
+					},
+					c = {
+						j = {
+							k = "<Esc>",
+							j = "<Esc>",
+						},
+					},
+				},
+			})
 		end,
 	},
 }
